e8bf58e1744bb98a68a9e4796f463a56d3efc55a
Brany Suero <branysuero@gmail.com>
2018-08-28 22:05:34 -0400

    bug fixed

--- DocTemp-fe/src/app/app-routing/app.module.ts
+++ DocTemp-fe/src/app/app-routing/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing-routing.module';
 
 @NgModule({
   imports: [
